0d009aa8f553100d55b6ebac3e4d1d2a3cc56aeb
jack <shistonejack@gmail.com>
2019-09-01 09:53:35 +0800

    git分支

--- src/com/jack/service/CustomerService.java
+++ src/com/jack/service/CustomerService.java
@@ -20,7 +20,10 @@ public class CustomerService {
     }
     @PreDestroy //相当于destroy-method
     public void destroy(){
+
+        System.out.println ("git练习==============");
         System.out.println ("git主干合并");
-        
+        System.out.println ("分支练习");
+
     }
 }
